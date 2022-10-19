cbab05fe4b85a2346d517c2937f0b496f198b7f7
Gavin <gavinporlich@gmail.com>
2022-10-19 18:02:53 -0500

    User, Thought, Reaction models started/Associations made

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const { Schema } = require("mongoose");
+
+// Schema to create Thought model
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
